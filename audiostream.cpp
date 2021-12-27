4be65e68e3e88e41f8e136d67eac2d56802aca1a
eramne <79015185+eramne@users.noreply.github.com>
2021-12-27 16:57:57 -0500

    a major

--- audiostream.cpp
+++ audiostream.cpp
@@ -11,12 +11,18 @@ AudioStream::~AudioStream()
 }
 
 qint64 AudioStream::readData(char *data, qint64 maxSize) {
-    qreal frequency = 440;
+    qreal frequency = 440; //standard A4
+    qreal volume = 1;
 
     int i = 0;
     while (i < maxSize) {
         qreal t = samplesProcessed/qreal(audio->format().sampleRate());
-        qint16 value = (qSin(M_PI * frequency * t))*32767;
+
+        qreal osc1 = qSin(M_PI * frequency * t);
+        qreal osc2 = qSin(M_PI * (frequency*qPow(2.0,400.0/1200.0)) * t); //400 cents = major third
+        qreal osc3 = qSin(M_PI * (frequency*qPow(2.0,700.0/1200.0)) * t); //700 cents = perfect fifth
+
+        qint16 value = qMax(qMin(((osc1+osc2+osc3)/3)*32767*volume,32767.0),-32767.0);
 
         data[i] = (char) (value & 0xff);
         data[i+1] = (char) ((value >> 8) & 0xff);
--- testaudiothing.cpp
+++ testaudiothing.cpp
@@ -5,7 +5,6 @@ TestAudioThing::TestAudioThing(QObject *parent) : QObject(parent)
 
     QAudioDevice info(QMediaDevices::defaultAudioOutput());
     QAudioFormat format = info.preferredFormat();
-    format.setSampleRate(44100);
     format.setSampleFormat(QAudioFormat::Int16);
 
     if (!info.isFormatSupported(format)) {
@@ -16,15 +15,6 @@ TestAudioThing::TestAudioThing(QObject *parent) : QObject(parent)
     audio = new QAudioSink(format, this);
     data = new AudioStream(this, audio);
     data->open(QIODevice::ReadWrite);
-    QObject::connect(audio, &QAudioSink::stateChanged, this, [=](){
-        qDebug() << audio->state() << audio->error() << audio->bufferSize();
-    });
     audio->setBufferSize(2);
     audio->start(data);
-
-    QTimer *timer = new QTimer(this);
-    QObject::connect(timer, &QTimer::timeout, this, [=](){
-        qDebug() << audio->state() << audio->error() << audio->bufferSize() << data->size() << audio->bytesFree() << data->isOpen() << data->isReadable() << data->pos() << audio->processedUSecs() << format.sampleFormat();
-    });
-    timer->start(1000);
 }
